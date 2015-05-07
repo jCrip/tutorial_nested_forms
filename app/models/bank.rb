2512c641b75457fb805d016ccb5f18ec24871b69
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-05-06 22:57:06 -0300

    add to_s method to models

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,4 +1,8 @@
 class Bank < ActiveRecord::Base
   has_many :bank_subsidiaries, dependent: :destroy
   validates :name, presence: true
+
+  def to_s
+    name
+  end
 end
--- app/models/bank_subsidiary.rb
+++ app/models/bank_subsidiary.rb
@@ -1,4 +1,8 @@
 class BankSubsidiary < ActiveRecord::Base
   belongs_to :bank
   validates :address, presence: true
+
+  def to_s
+    address
+  end
 end
