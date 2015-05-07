d3db800b139594cdbda6b358355bcc4b7bb39d0d
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-05-06 23:31:38 -0300

    add accept nested attributes

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,5 +1,8 @@
 class Bank < ActiveRecord::Base
   has_many :bank_subsidiaries, dependent: :destroy
+
+  accepts_nested_attributes_for :bank_subsidiaries
+
   validates :name, presence: true
 
   def to_s
